a67451d70b6e1617ce783c08e54d4011cfcfc470
rajatverma491 <rajatverma491@gmail.com>
2020-08-29 10:44:53 +0530

    first commError Resolved

---
+++ WebChecking/WebChecking/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebChecking.Models;
+using WebChecking.ViewModel;
+
+namespace WebChecking.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext _context;
+		public EmployeeController()
+		{
+            _context = new ApplicationDbContext();
+		}
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index","Home");
+        }
+
+        public ActionResult Create()
+        {
+            var departments = _context.Departments.ToList();
+            var dropDepartment = new EmployeeDepartmentViewModel
+            {
+                Departments=departments
+            };
+            return View(dropDepartment);
+        }
+        [HttpPost]
+
+        public ActionResult Create(EmployeeDepartmentViewModel employeeDepartmentViewModel )
+        {
+            Employee employee = new Employee
+            {
+                EName=employeeDepartmentViewModel.Employee.EName,
+                DepartmentId=employeeDepartmentViewModel.Department.Id
+
+            };
+           _context.Employees.Add(employee);
+           _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+        
+    }
+}
---
+++ WebChecking/WebChecking/ViewModel/EmployeeDepartmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebChecking.Models;
+
+namespace WebChecking.ViewModel
+{
+	public class EmployeeDepartmentViewModel
+	{
+		public IEnumerable<Department> Departments { get; set; }
+		public Employee Employee { get; set; }
+        public Department Department { get; set; }
+    }
+}
